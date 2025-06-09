0819a5b137a16d06a7eef79e25a74fdf9fbd01f3
Avazbek <avazsobirov9966@gmail.com>
2025-06-09 22:22:22 +0500

    Add class

--- src/component/HeroSection/SubHeroSection.jsx
+++ src/component/HeroSection/SubHeroSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const SubHeroSection = () => {
   return (
     <div className='flex items-center justify-around w-full gap-4 py-8 uppercase border border-y-lightGrey xl:text-4xl md:text-2xl sm:text-4xl bg-brown text-lightGrey '>
-      <p>Fast Learner</p>
-      <p>Team Work</p>
-      <p>Detalis Master</p>
+      <p className='md:block sm:hidden'>Fast Learner</p>
+      <p className='md:block sm:hidden'>Team Work</p>
+      <p >Detalis Master</p>
     </div>
   )
 }
