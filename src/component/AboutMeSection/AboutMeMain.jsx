9c15bab5cfb04a8a688f81fc87d73d59a2e3c2fd
Avazbek <avazsobirov9966@gmail.com>
2025-06-10 21:02:03 +0500

    biroz code yozdim

--- src/component/AboutMeSection/AboutMeMain.jsx
+++ src/component/AboutMeSection/AboutMeMain.jsx
@@ -4,7 +4,7 @@ import AboutMeImage from './AboutMeImage'
 
 const AboutMeMain = () => {
   return (
-    <div>
+    <div className='flex gap-12 p-4 md:flex-row sm:flex-col max-w-[1200px] mx-auto mt-[100px] justify-between items-center'>
       <AboutMeText/>
       <AboutMeImage/>
     </div>
--- src/component/AboutMeSection/AboutMeText.jsx
+++ src/component/AboutMeSection/AboutMeText.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const AboutMeText = () => {
   return (
     <div>
-      
+      <h2>About Me</h2>
+      <p>
+        My name is Avazbek, I have been interested in programming since 2017 and
+        currently I am a frontend developer, I have been working with frontend
+        for 2 years and now I have my own team and have done big projects with
+        them.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMeText
+export default AboutMeText;
