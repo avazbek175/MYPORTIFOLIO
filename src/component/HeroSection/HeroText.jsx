f118b38dd28cb44c9e125448f6d37251b55f7cca
Avazbek <avazsobirov9966@gmail.com>
2025-06-10 22:48:43 +0500

    biroz code yozdim

--- src/component/HeroSection/HeroText.jsx
+++ src/component/HeroSection/HeroText.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaTelegram } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const HeroText = () => {
   return (
@@ -7,6 +10,11 @@ const HeroText = () => {
       <h1 className='md:text-[2.8rem] lg:text-6xl sm:text-4xl font-bold font-special text-orange'>Avazbek Sobirov</h1>
       <p className='mt-4 text-lg text-white'>I have been working as a Frontend developer <br />
       for 2 years.</p>
+      <div className='flex items-center gap-5 mt-[10px]'>
+        <a href="https://t.me/as_exe/" ><FaTelegram className='text-3xl transition-all duration-500 hover:scale-90 hover:text-cyan'/></a>
+        <a href="https://instagram.com/avazbek_lll/" ><FaInstagram className='text-3xl transition-all duration-500 hover:scale-90 hover:text-cyan'/></a>
+        <a href="https://github.com/avazbek175/" ><FaGithub className='text-3xl transition-all duration-500 hover:scale-90 hover:text-cyan'/></a>
+      </div>
     </div>
   )
 }
