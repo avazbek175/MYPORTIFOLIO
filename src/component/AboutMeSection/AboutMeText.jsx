625a50968c3b396c91c27a99a27379aec2348b0a
Avazbek <avazsobirov9966@gmail.com>
2025-06-10 21:14:30 +0500

    biroz code yozdim

--- src/component/AboutMeSection/AboutMeText.jsx
+++ src/component/AboutMeSection/AboutMeText.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const AboutMeText = () => {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="mb-10 text-6xl text-cyan">About Me</h2>
+      <p className="text-white ">
         My name is Avazbek, I have been interested in programming since 2017 and
-        currently I am a frontend developer, I have been working with frontend
-        for 2 years and now I have my own team and have done big projects with
+        currently <br />I am a frontend developer, I have been working with frontend
+        for <br />2 years and now I have my own team and have done big projects with
         them.
       </p>
+      <button className="flex items-center px-4 py-2 mt-10 text-lg text-white transition-all duration-500 border rounded-full cursor-pointer border-orange hover:bg-orange md:self-start sm:self-center hover:text-cyan ">My Project</button>
     </div>
   );
 };
