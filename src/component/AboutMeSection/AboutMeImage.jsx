16d668a7a89619b18660b6b520ace303d04868a5
Avazbek <avazsobirov9966@gmail.com>
2025-06-10 22:25:33 +0500

    biroz code yozdim

--- src/component/AboutMeSection/AboutMeImage.jsx
+++ src/component/AboutMeSection/AboutMeImage.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const AboutMeImage = () => {
   return (
-    <div>
-      
+    <div className='h-[500px] w-[300px] relative'>
+      <div className='h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden'>
+      <img src="../../public/img/about-me.jpg" alt="About Images" className='object-cover w-auto h-full'/>
+      </div>
+      <div className='h-[500px] w-[250px] bottom-[-30px] bg-orange rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10 absolute  left-[-30px]'></div>
     </div>
+
   )
 }
 
--- src/component/AboutMeSection/AboutMeMain.jsx
+++ src/component/AboutMeSection/AboutMeMain.jsx
@@ -5,8 +5,12 @@ import AboutMeImage from './AboutMeImage'
 const AboutMeMain = () => {
   return (
     <div className='flex gap-12 p-4 md:flex-row sm:flex-col max-w-[1200px] mx-auto mt-[100px] justify-between items-center'>
+      <div>
       <AboutMeText/>
+      </div>
+      <div>
       <AboutMeImage/>
+      </div>
     </div>
   )
 }
